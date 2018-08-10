6ceea3e5145791f229c1c3316eb15a0a32be0f9e
aparecida <aparecidapiresbarb@gmail.com>
2018-08-09 21:36:32 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    FormsModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public edit = false;
 
+  categoryName: string;
+  categoriesCollection: AngularFirestoreCollection<any> = this.afs.collection('categories');
+  categoriesList = this.categoriesCollection.valueChanges();
+  idCategory: string;
+
+  constructor(public navCtrl: NavController, public afs: AngularFirestore) { }
+
+  ionViewDidLoad() { }
+
+  categoryData() {
+    return this.categoriesList.subscribe(category => {
+      category.forEach(res => {
+        this.idCategory = res.id,
+          this.categoryName = res.name
+      })
+    })
+  }
+
+  add() {
+    this.idCategory = this.afs.createId();
+    this.categoriesCollection.doc(this.idCategory).set({
+      id: this.idCategory,
+      name: this.categoryName
+    }).then(() => {
+      this.categoryName = ''
+    })
+  }
+
+  save() {
+    this.categoriesCollection.doc(this.idCategory).update({
+      name: this.categoryName
+    }).then(() => {
+      this.categoryName = ''
+    })
+    this.edit = false;
+  }
+
+  update(id, name) {
+    this.categoryName = name
+    this.edit = true
+  }
+
+  delete(id) {
+    this.categoriesCollection.doc(id).delete()
   }
 
 }
